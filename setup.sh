38792f37266f3bb0a16877473cd266ffb4d50b39
chizo <fjcierkosz1@sheffield.ac.uk>
2024-03-04 19:35:42 +0000

    Create a simpler more unified script.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The name of the file to save grades.
-filename="data/academic_module_grades.csv"
-
-# Create the file and write the header.
-echo "code,name,credits,fheq,grade" > "$filename"
-
-echo "CSV data file created with a header. It can be found in path: ./"$filename
